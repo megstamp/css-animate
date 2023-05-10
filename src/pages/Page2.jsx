53b9c22df37dc537833c8963611911683b34ec5c
Meghann Stamper <meg.stamp@yahoo.com>
2023-05-10 16:59:40 -0400

    more css

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -1,10 +1,34 @@
+import { Container, Row, Col} from "react-bootstrap";
+import "../styles/Page2.css"
 
 export default function Page2() {
 
+    const imgRoot = process.env.PUBLIC_URL + "/images";
 
-    return(
-        <>
-            <h2>Page2</h2>
-        </>
-    )
+    return (
+       <section>
+        <Container>
+             <Row>
+                <Col><button class="pg2-button button1">X axis</button></Col>
+                 <Col><button class="pg2-button button2">Y axis</button></Col>
+                 <Col><button class="pg2-button button3">Scale</button></Col>
+             </Row>
+
+             <Row>
+                <Col>
+                <div className="profile">
+                    <img src={imgRoot+"/pict.webp"} alt="mermaid" />
+
+                 <div className ="profile-text">
+                    <h3>Mermaid time</h3>
+                    <a href="#" class="profile-button" rel="noreferrer">Ariel</a>
+                    </div>   
+                </div>
+                </Col>
+             </Row>
+       </Container>
+       
+       
+       </section>
+ )
 }
